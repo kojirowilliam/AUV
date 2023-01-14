8f219810088f9844564e5ca7ed54473c2ba03df8
Kojiro Yamada <kojirowilliamrocks@gmail.com>
2023-01-14 09:42:52 -0800

    Initial commit for zed_docker

--- Docker/gazebo_docker/build_gazebo.sh
+++ Docker/gazebo_docker/build_gazebo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME="my-gazebo-app"
-tag="0"
-docker image ls
+docker image ls $NAME
 echo "What do you want to tag this?"
+read tag
 docker build -t $NAME:$tag .
 echo "Built $NAME:$tag"
