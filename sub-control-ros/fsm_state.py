bbcdd60b16341c65bcca638d72bfa61830266b5e
Kojiro Yamada <kojirowilliamrocks@gmail.com>
2023-07-15 14:13:23 -0700

    Created a package that makes it easier to control the sub using the keyboard without a need for sudo priviledges. Also started creating thrusters class and quaternion PID.

--- sub-control-ros/fsm_state.py
+++ sub-control-ros/fsm_state.py
@@ -1,19 +1,25 @@
-class fsm_state():
-    def __init__(self):
-        pass
+from abc import ABC, abstractmethod
 
+class fsm_state(ABC):
     def get_name(self):
         return self.name
 
+    @abstractmethod
     def enter(self):
         pass
+
+    @abstractmethod
     def run(self, dt):
-         raise NotImplementedError("run function not implemented")
+        pass
+    
+    @abstractmethod
     def exit(self):
         pass
 
+    @abstractmethod
     def set_rot_target(self, r, p, y):
         pass
 
+    @abstractmethod
     def get_thrust_list(self):
         pass
